e657859da1801f584d2c60d7a24e2afdab029545
dkeith <dkeith@mindboxstudios.com>
2015-07-13 03:12:46 -0500

    ...

--- js/phenogridloader-no-species.js
+++ js/phenogridloader-no-species.js
@@ -13,20 +13,26 @@ function loadPhenogrid(){
         var slash_idx = disease_id.indexOf('/');
         disease_id = disease_id.substring(slash_idx+1);
         var phenotype_list = [];
+        var phenogridContainer = document.getElementById('phen_vis');
         jQuery.ajax({
             url : '/' + disease_id + '/phenotype_list.json',
-            async : false,
+            async : true,
             dataType : 'json',
+            timeout : 180000,
+            error : function(jqXHR, textStatus, errorThrown) {
+                var phenogridOpts = {
+                                        phenotypeData: phenotype_list
+                                    };
+                Phenogrid.createPhenogridForElement(phenogridContainer, phenogridOpts);
+            },
             success : function(data) {
                 phenotype_list = data.phenotype_list;
+
+                var phenogridOpts = {
+                                        phenotypeData: phenotype_list
+                                    };
+                Phenogrid.createPhenogridForElement(phenogridContainer, phenogridOpts);
             }
         });
-
-        var phenogridContainer = document.getElementById('phen_vis');
-        var phenogridOpts = {
-                                phenotypeData: phenotype_list
-                            };
-
-        Phenogrid.createPhenogridForElement(phenogridContainer, phenogridOpts);
     }
 }
